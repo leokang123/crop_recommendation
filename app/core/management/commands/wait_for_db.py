27cd47221d2ba624bc6d2eb787d41b84d683502a
leokang123 <leokang123@github.com>
2025-02-03 19:25:28 +0900

    Fixed flake8 config

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,5 +1,5 @@
 """
-Django command to wait for the database available 
+Django command to wait for the database available
 """
 
 import time
@@ -14,14 +14,13 @@ class Command(BaseCommand):
     """Django command to wait for command"""
 
     def handle(self, *args, **opt):
-        self.stdout.write('Waiting for database')
+        self.stdout.write("Waiting for database")
         db_up = False
         while db_up is False:
             try:
-                self.check(databases=['default'])
+                self.check(databases=["default"])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write(
-                    "Database unavailable, waiting 1 second ... ")
+                self.stdout.write("Database unavailable, waiting 1 second ....")
                 time.sleep(1)
-        self.stdout.write(self.style.SUCCESS('Database Avaiable!'))
+        self.stdout.write(self.style.SUCCESS("Database Avaiable!"))
